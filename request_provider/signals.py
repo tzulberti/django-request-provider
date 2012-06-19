279ad6e98e778fef39094253e9713cf3aeee5eb0
Tomas Zulberti <tzulberti@gmail.com>
2012-06-19 15:43:53 -0300

    Se hizo un setup inicial

---
+++ request_provider/signals.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from django.dispatch import Signal
+
+__doc__="""
+If you want to get a reference to the current HttpRequest:
+
+from request_provider.signals import get_request
+http_request = get_request()
+
+"""
+
+class UnauthorizedSignalReceiver(Exception):
+    pass
+
+class SingleHandlerSignal(Signal):
+
+    allowed_receiver='request_provider.middleware.RequestProvider'
+
+    def __init__(self,providing_args = None):
+        return Signal.__init__(self,providing_args)
+
+    def connect(self, receiver, sender=None, weak=True, dispatch_uid=None):
+        receiver_name = '.'.join([receiver.__class__.__module__,
+            receiver.__class__.__name__])
+        if receiver_name != self.allowed_receiver:
+            raise UnauthorizedSignalReceiver()
+        Signal.connect(self, receiver, sender, weak, dispatch_uid)
+
+
+request_accessor = SingleHandlerSignal()
+
+
+def get_request():
+    signal_response = request_accessor.send(None)
+    if signal_response:
+        return None
+    return signal_response[0][1]
+
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-request-provider",
+    install_requires = [
+        'django',
+    ],
+    packages = find_packages('.'),
+    package_dir = {'':'.'},
+    version = "1.0.0",
+    description = "access django request object whenever you need it",
+    long_description=open('README.md').read(-1),
+    author = "Tomas Zulberti",
+    author_email = "tzulberti@gmail.com",
+    url = "http://github.com/tzulberti/django-request-provider",
+    keywords = [
+        "django contrib",
+        "request object provider"
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent'
+        ],
+    license = 'License :: OSI Approved :: BSD License',
+)
