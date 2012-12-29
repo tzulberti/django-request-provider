8e2d11ad6fd34e9878ba2f23144320d0a317e541
Tomas Zulberti <tzulberti@gmail.com>
2012-12-29 12:36:45 -0300

    Improved pep8, solved bug when returning request

--- request_provider/signals.py
+++ request_provider/signals.py
@@ -17,15 +17,16 @@ class SingleHandlerSignal(Signal):
 
     allowed_receiver='request_provider.middleware.RequestProvider'
 
-    def __init__(self,providing_args = None):
-        return Signal.__init__(self,providing_args)
+    def __init__(self, providing_args=None):
+        return super(SingleHandlerSignal, self).__init__(providing_args)
 
     def connect(self, receiver, sender=None, weak=True, dispatch_uid=None):
         receiver_name = '.'.join([receiver.__class__.__module__,
             receiver.__class__.__name__])
         if receiver_name != self.allowed_receiver:
             raise UnauthorizedSignalReceiver()
-        Signal.connect(self, receiver, sender, weak, dispatch_uid)
+        super(SingleHandlerSignal, self).connect(receiver, sender, weak,
+                                                dispatch_uid)
 
 
 request_accessor = SingleHandlerSignal()
@@ -33,7 +34,7 @@ request_accessor = SingleHandlerSignal()
 
 def get_request():
     signal_response = request_accessor.send(None)
-    if signal_response:
+    if not signal_response:
         return None
     return signal_response[0][1]
 
